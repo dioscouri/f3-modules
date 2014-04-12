5ad59e9833d4f866887b34ba1469d6a5a9a8de18
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-04-11 22:42:44 -0400

    added wysiwyg editor to menu item html module

--- src/Modules/Admin/Views/menuitem/html.php
+++ src/Modules/Admin/Views/menuitem/html.php
@@ -1,4 +1,11 @@
+<script src="./ckeditor/ckeditor.js"></script>
+<script>
+jQuery(document).ready(function(){
+    CKEDITOR.replaceAll( 'wysiwyg' );    
+});
+</script>
+
 <p class="">
     <label for="link-content" class="control-label">Content</label>
-    <textarea name="details[content]" class="form-control" placeholder="Insert HTML Here" rows="5" ><?php echo htmlspecialchars( $item->{'details.content'} ); ?></textarea>
+    <textarea name="details[content]" class="wysiwyg form-control" placeholder="Insert HTML Here" rows="5" ><?php echo htmlspecialchars( $item->{'details.content'} ); ?></textarea>
 </p>

7e2165bf33594cc407037b89c14df0b46c01b221
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-06-10 23:58:58 -0400

    fixed html methods

--- src/Modules/Modules/Megamenu/Module.php
+++ src/Modules/Modules/Megamenu/Module.php
@@ -70,18 +70,10 @@ class Module extends \Modules\Abstracts\Module
     
     public function html()
     {
-        $f3 = \Base::instance();
-        
-        $old_ui = $f3->get('UI');
-        $temp_ui = !empty($this->options['views']) ? $this->options['views'] : dirname( __FILE__ ) . "/Views/";
-        $f3->set('UI', $temp_ui);
-        
-        $f3->set('module', $this);
+        \Base::instance()->set('module', $this);
         
         \Dsc\System::instance()->get('theme')->registerViewPath( __dir__ . '/Views/', 'Modules/Megamenu/Views' );
-        $string = \Dsc\System::instance()->get('theme')->renderLayout('Modules/Megamenu/Views::default.php');        
-        
-        $f3->set('UI', $old_ui);
+        $string = \Dsc\System::instance()->get('theme')->renderLayout('Modules/Megamenu/Views::default.php');
         
         return $string;
     }
